dd5b59be03aab577c4c64446fb1a980292e9596d
Evgeny Sinelnikov <sin@altlinux.ru>
2017-11-26 06:17:36 +0400

    Initial Makefile for Arduino projects

---
+++ build/libraries/AltSoftSerial/AltSoftSerial.cpp.d
@@ -0,0 +1,16 @@
+/home/connor/oss/megaobd/build/libraries/AltSoftSerial/AltSoftSerial.cpp.o: \
+ /home/connor/oss/megaobd/libraries/AltSoftSerial/AltSoftSerial.cpp \
+ /home/connor/oss/megaobd/libraries/AltSoftSerial/AltSoftSerial.h \
+ /home/connor/arduino-1.8.5/hardware/arduino/avr/cores/arduino/Arduino.h \
+ /home/connor/arduino-1.8.5/hardware/arduino/avr/cores/arduino/binary.h \
+ /home/connor/arduino-1.8.5/hardware/arduino/avr/cores/arduino/WCharacter.h \
+ /home/connor/arduino-1.8.5/hardware/arduino/avr/cores/arduino/WString.h \
+ /home/connor/arduino-1.8.5/hardware/arduino/avr/cores/arduino/HardwareSerial.h \
+ /home/connor/arduino-1.8.5/hardware/arduino/avr/cores/arduino/Stream.h \
+ /home/connor/arduino-1.8.5/hardware/arduino/avr/cores/arduino/Print.h \
+ /home/connor/arduino-1.8.5/hardware/arduino/avr/cores/arduino/Printable.h \
+ /home/connor/arduino-1.8.5/hardware/arduino/avr/cores/arduino/USBAPI.h \
+ /home/connor/arduino-1.8.5/hardware/arduino/avr/cores/arduino/Arduino.h \
+ /home/connor/arduino-1.8.5/hardware/arduino/avr/variants/standard/pins_arduino.h \
+ /home/connor/oss/megaobd/libraries/AltSoftSerial/config/AltSoftSerial_Boards.h \
+ /home/connor/oss/megaobd/libraries/AltSoftSerial/config/AltSoftSerial_Timers.h
